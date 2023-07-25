9d7726ec93b54b19ad010877487435dc01aa7ac8
johngao01 <1649868243@qq.com>
2023-07-25 22:19:50 +0800

    打包文件夹中的文件，节省空间

---
+++ package.py
@@ -0,0 +1,25 @@
+import os
+import time
+import zipfile
+
+folder = r'/root/download/weibo/'
+
+z = zipfile.ZipFile("/media/media.zip", 'a')
+os.chdir(folder)
+
+start = time.time()
+count = 0
+for root, dirs, files in os.walk(folder):
+    for file in files:
+        file_path = os.path.join(root, file)
+        if file_path.endswith("csv"):
+            continue
+        count += 1
+        relative_path = os.path.relpath(file_path, folder)
+        print(count, relative_path)
+        z.write(file_path, relative_path)
+        os.remove(file_path)
+z.close()
+end = time.time()
+
+print(f"Cost {end - start} second")
